87f3a816de14701272622d980d45b8af0e70aed4
Darshana Ghodasaini <darshanaghodasaini106@gmail.com>
2025-10-08 07:46:26 +0545

    Added hello.c and hello.cpp programs

---
+++ hello.c
@@ -0,0 +1,6 @@
+﻿#include <stdio.h>
+
+int main() {
+    printf("Hello, World from C!\n");
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World from C++!" << endl;
+    return 0;
+}
